56aff828302afa52648e92b6d330d515dab9b0b6
Randy <randy.lai@egoal-tech.com>
2015-09-26 17:38:59 +0800

    add

---
+++ OrderManager.Manager/Common/StaticResource.cs
@@ -0,0 +1,15 @@
+﻿using OrderManager.Model.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OrderManager.Manager
+{
+
+
+ public class StaticResource
+ {
+     public static List<OM_ProductPrice> UserProductPrices { get; set; }
+ }
+
+
+}
